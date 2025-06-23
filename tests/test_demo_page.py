2b845a2ebfee0880205ee98eb7521bb6c652de4c
AyalaDruk <ayalahorberg1999@gmail.com>
2025-06-23 21:22:30 +0300

    playwright html and page

---
+++ tests/test_demo_page.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page,expect
+
+
+
+BASE_URL="https://devexpress.github.io/testcafe/example/"
+
+def test_demo_page_loaded(page:Page):
+ page.goto(BASE_URL)
+ # assert page.url == BASE_URL+"/home-page"
+ expect(page).to_have_url(BASE_URL, timeout=7500)
+
+
+def test_demo_page_title(page: Page):
+    page.goto(BASE_URL)
+    expect(page).to_have_title("TestCafe Example Page")
+
+
+def test_user_name_presented(page: Page):
+    pass
