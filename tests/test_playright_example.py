d88e17e858933036c1af39f6cfe9092a16007d80
AyalaDruk <ayalahorberg1999@gmail.com>
2025-06-24 21:17:48 +0300

    playwright html and page

--- tests/test_playright_example.py
+++ tests/test_playright_example.py
@@ -1,5 +1,12 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, BrowserContext, APIRequestContext
+
+
+# Structure
+#
+# Playwright
+# Browser + Context
+# Page - tab inside browser - Incognito mode
 
 def test_has_title(page: Page):
 
@@ -12,6 +19,18 @@ def test_has_title(page: Page):
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+def test_example(page: Page, context: BrowserContext, api_context: APIRequestContext):
+    page.goto("https://playwright.dev/")
+    context.clear_cookies()
+
+
 
     # Click the get started link.
     page.get_by_role("link", name="Get started").click()
